6b9ab2babad609cfd4443156670de50d9cb28947
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-12-01 16:15:14 +0100

    Implement automigration

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import mikroOrmConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
-
+  await orm.getMigrator().up();
   const post = orm.em.create(Post, { title: 'My First Post' });
   await orm.em.persistAndFlush(post);
 };
 
-main().catch(err => console.error(err))
+main().catch(err => console.error(err));
