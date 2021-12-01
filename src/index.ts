8423ed3c6f109ed1213710427700305ebd1f63f0
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-12-01 16:16:38 +0100

    Comment post creation in MikroOrm

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { MikroORM } from '@mikro-orm/core';
-import { Post } from './entities/Posts';
 import mikroOrmConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: 'My First Post' });
-  await orm.em.persistAndFlush(post);
+  // const post = orm.em.create(Post, { title: 'My First Post' });
+  // await orm.em.persistAndFlush(post);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 
 main().catch(err => console.error(err));
