02a97a69bd150d3e937d6b2c93bf657a76fe9b3d
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-12-01 16:25:50 +0100

    Make basic express setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { MikroORM } from '@mikro-orm/core';
+import express from 'express';
 import mikroOrmConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: 'My First Post' });
-  // await orm.em.persistAndFlush(post);
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+
+  const app = express();
+
+  app.listen(4000, () => {
+    console.log('Server is started on 4000');
+  });
 };
 
 main().catch(err => console.error(err));
