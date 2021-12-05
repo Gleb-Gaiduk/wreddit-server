d5205724028cc675df60732edb8550dd9062eab1
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-12-05 09:22:45 +0100

    Extend types of express session

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
 import { Connection, EntityManager, IDatabaseDriver } from '@mikro-orm/core';
 import { Request, Response } from 'express';
 
+declare module 'express-session' {
+  export interface SessionData {
+    userId: number;
+  }
+}
+
 export type TMyContext = {
   em: EntityManager<IDatabaseDriver<Connection>>;
   req: Request;
