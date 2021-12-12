f5af3cbb0e908fa47ebe81ea79e98180fbbabb57
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-12-12 12:23:55 +0100

    Separate register validation

---
+++ src/utils/validateRegister.ts
@@ -0,0 +1,41 @@
+import { UsernamePasswordInput } from 'src/utils/UsernamePasswordInput';
+
+export const validateRegister = (options: UsernamePasswordInput) => {
+  if (!options.email.includes('@')) {
+    return [
+      {
+        field: 'email',
+        message: 'invalid email',
+      },
+    ];
+  }
+
+  if (options.username.length <= 2) {
+    return [
+      {
+        field: 'username',
+        message: 'username lenght might be greater than 2',
+      },
+    ];
+  }
+
+  if (options.password.length < 8) {
+    return [
+      {
+        field: 'password',
+        message: 'password length might be at least 8 characters',
+      },
+    ];
+  }
+
+  if (options.username.includes('@')) {
+    return [
+      {
+        field: 'username',
+        message: 'cannot include @ sign',
+      },
+    ];
+  }
+
+  return null;
+};
