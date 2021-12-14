20c3be1c9dd99d9358211ffe0773510fae69ac55
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2021-12-14 19:40:09 +0100

    6.54.00 | Fill in DB with fake data and do migrations

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import { ApolloServer } from 'apollo-server-express';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
-// import connectRedis from 'connect-redis';
 import express from 'express';
 import session from 'express-session';
 import Redis from 'ioredis';
 import 'reflect-metadata';
-// import { createClient } from 'redis';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
 import { COOKIE_NAME, __prod__ } from './constants';
@@ -26,7 +24,9 @@ const main = async () => {
     logging: true,
     synchronize: true,
     entities: [Post, User],
+    // migrations: [path.join(__dirname, './migrations/*')],
   });
+  // await conn.runMigrations();
 
   const app = express();
 
